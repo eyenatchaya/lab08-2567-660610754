4e35413b15c45bd567ad1809be4d09874fb2a89f
eye <eyenathaya5@gmail.com>
2024-07-19 17:36:24 +0700

    complete

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -1,15 +1,16 @@
-import React from "react"
-const decoration = (isDone) => {
-  return isDone ? "line-through" : "";
+import React from 'react'
+
+const status = (isDone) => {
+    if(isDone) return "line-through";
+    else return "";
 };
-const Task = (props) => {
+
+export default function Task(props) {
   return (
-    <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      <span style={{ textDecoration: decoration(props.isDone) }}>
-        {props.title}
-      </span>
-      <button className="btn btn-success">Done</button>
-      <button className="btn btn-danger">Delete</button>
-    </div>
-  );
-};
+        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+          <span style={{textDecoration: status(props.isDone)}}>{props.title}</span>
+          <button className="btn btn-success">Done</button>
+          <button className="btn btn-danger">Delete</button>
+        </div>
+  )
+}
